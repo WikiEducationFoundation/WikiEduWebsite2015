849aef4546d799829b084eddcdaba40f020d35b0
kevinbleich <kevin@wintr.us>
2015-05-20 11:38:27 -0700

    toc styles and team-staff styles

--- page-for-instructors.php
+++ page-for-instructors.php
@@ -5,6 +5,26 @@
 
   <div class="page-content row">
    <div class="col-sm-6 col-sm-push-1">
+
+    <div class="toc-block">
+      <h3>Table of Contents</h3>
+
+        <ul>
+          <li>1. <a href="#">Subject-specific handouts</a></li>
+          <li>2. <a href="#">Classroom handouts</a></li>
+          <li>3. <a href="#">Selecting article topics</a></li>
+          <li>4. <a href="#">Course pages</a></li>
+        </ul>
+    
+  
+        <ul>
+          <li>5. <a href="#basics">Learn the basics</a></li>
+          <li>6. <a href="#">Assignment planning</a></li>
+          <li>7. <a href="#">Motivating students</a></li>
+          <li>8. <a href="#">Teaching tools</a></li>
+        </ul>
+     
+    </div>
       <?php get_template_part('templates/content', 'page'); ?>
    </div>
   </div>
--- templates/hero.php
+++ templates/hero.php
@@ -1,5 +1,7 @@
-<div class="hero">
-   <div class="hero-inner">
-      
+
+<?php $featured_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+<div class="hero" style="background:whitesmoke url(<?= $featured_image; ?>) no-repeat center center; background-size: cover;">
+   <div class="hero-inner" >
+    
    </div>
 </div>
