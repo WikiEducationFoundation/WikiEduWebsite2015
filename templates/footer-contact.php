3e52e19b25ea4d11966d60facaf3f9ce54c7bd3e
Sage Ross <ragesoss@gmail.com>
2015-06-04 09:51:54 -0700

    Fix mislabeled Twitter handle

--- templates/footer-contact.php
+++ templates/footer-contact.php
@@ -2,5 +2,5 @@
  <p class="small">
     San Francisco, CA <br/>
     <a href="mailto:contact@wikiedu.org" target="_blank">contact@wikiedu.org</a> <br/>
-    <a href="http://www.twitter.com/WikiEducation">@WikiFoundation</a>
+    <a href="http://www.twitter.com/WikiEducation">@WikiEducation</a>
  </p>
