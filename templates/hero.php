07f1553badb238f492db43ef5db5c6db9026ba79
kevinbleich <kevin@wintr.us>
2015-06-15 16:22:25 -0700

    update

--- templates/hero.php
+++ templates/hero.php
@@ -1,10 +1,13 @@
 <?php use Roots\Sage\Titles; ?>
 <?php 
+$hero_class = 'no-image';
+$featured_image = '';
 if(has_post_thumbnail()) {
   $featured_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); 
+  $hero_class = '';
   } 
 ?>
-<div class="hero" style="background-image:url(<?= $featured_image; ?>);background-position: 50% 0%;">
+<div class="hero <?= $hero_class?>" style="background-image:url(<?= $featured_image; ?>);background-position: 50% 0%;">
   <?= the_post_thumbnail(); ?>
   <div class="hero-gradient"></div>
   <div class="hero-gradient hero-gradient--white"></div>
