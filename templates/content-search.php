44ca988219ded064047886c90bc6b54ab4fdd487
Sage Ross <ragesoss@gmail.com>
2016-03-30 12:27:33 -0700

    search both posts and pages

--- templates/content-search.php
+++ templates/content-search.php
@@ -1,6 +1,6 @@
 <article <?php post_class(); ?>>
   <header>
-   <?php if (get_post_type() === 'post') { ?>
+   <?php if (in_array(get_post_type(), ['post','page'])) { ?>
       <h4 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
       <p class="entry-date"><?php if (get_post_type() === 'post') { the_date(); } ?></p>
    <?php } ?>
