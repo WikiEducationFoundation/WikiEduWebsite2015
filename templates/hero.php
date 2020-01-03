2d6de3ad283fd3af8853199f8ea0719fdde16d1c
Sage Ross <ragesoss@gmail.com>
2020-01-03 09:39:26 -0800

    tweak to hide hero section for plain pages without thumbnails
    I'm working on a new homepage, and I want to minimize the theme customizations so that nearly all the page can be controlled from the page body. This is a workaround to get rid of the unwanted hero section.

--- templates/hero.php
+++ templates/hero.php
@@ -6,6 +6,9 @@ if(has_post_thumbnail()) {
   $featured_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
   $hero_class = '';
 }
+else if (is_page()) {
+  $hero_class = 'hidden';
+}
 
 if (get_post_meta($post->ID, 'image_focal_point', true)) {
   $image_focal_point = get_post_meta($post->ID, 'image_focal_point', true);
